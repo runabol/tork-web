ce351ff84b88daf0a2ef484e67da480a10608c41
Arik Cohen <me@arikcohen.com>
2023-08-23 00:50:03 -0400

    got rid of unncessary headings

--- app/jobs/create/page.tsx
+++ app/jobs/create/page.tsx
@@ -20,13 +20,6 @@ tasks:
   return (
     <>
       <main className="py-6">
-        <div className="sm:flex sm:items-center">
-          <div className="sm:flex-auto">
-            <h1 className="text-2xl font-semibold leading-6 text-gray-900">
-              New Job
-            </h1>
-          </div>
-        </div>
         <div className="mt-8 flow-root">
           <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
@@ -48,7 +41,7 @@ tasks:
                 />
               </div>
               <Alert message={errorMsg} />
-              <div className="flex gap-2 mt-2 justify-end">
+              <div className="flex gap-2 mt-4 justify-end">
                 <button
                   type="button"
                   className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -7,29 +7,6 @@ export default async function Jobs() {
   return (
     <>
       <main className="py-6">
-        <div className="md:flex md:items-center md:justify-between">
-          <div className="min-w-0 flex-1">
-            <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-              Recent Jobs
-            </h2>
-          </div>
-          <div className="mt-4 flex md:ml-4 md:mt-0">
-            {/* <button
-                type="button"
-                className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-              >
-                Edit
-              </button> */}
-            {/* <Link href={"/jobs/create"}>
-                <button
-                  type="button"
-                  className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  New Job
-                </button>
-              </Link> */}
-          </div>
-        </div>
         <div className="mt-8 flow-root">
           <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
