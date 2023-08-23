865a696ad714808fc6d02f4d4917e4ad03391f4c
Arik Cohen <me@arikcohen.com>
2023-08-23 00:56:04 -0400

    Added menu items

--- components/header.tsx
+++ components/header.tsx
@@ -11,12 +11,23 @@ export default function Header() {
         <div className="flex h-16 justify-between">
           <div className="flex">
             <div className="hidden md:flex md:space-x-8">
-              {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
               <a
                 href="/"
                 className="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900"
               >
                 Recent Jobs
+              </a>
+              <a
+                href="#"
+                className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+              >
+                Queues
+              </a>
+              <a
+                href="#"
+                className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+              >
+                Nodes
               </a>
             </div>
           </div>
