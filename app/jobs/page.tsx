0d9eeb304c29ce0227c7ef19390e14dc00ca5695
Arik Cohen <me@arikcohen.com>
2023-08-25 02:08:17 -0400

    renamed sub-job to sub

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -36,7 +36,7 @@ export default async function Jobs({
                 <span>{item.name}</span>
                 {item.parentId ? (
                   <span className="inline-flex items-center rounded-md bg-gray-200 px-2 py-1 text-xs font-medium text-gray-700 ring-1 ring-inset ring-gray-500/10">
-                    sub-job
+                    sub
                   </span>
                 ) : (
                   <></>
