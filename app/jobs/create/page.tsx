3aba625f9a2b1883cab7ef10b6cad9809f5ca986
Arik Cohen <me@arikcohen.com>
2023-08-24 21:39:11 -0400

    cleaned up styling

--- app/jobs/create/page.tsx
+++ app/jobs/create/page.tsx
@@ -27,7 +27,6 @@ tasks:
               language="yaml"
               theme="vs-dark"
               value={content}
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6 font-mono"
               onChange={(value) => setContent(value || "")}
             />
             <Alert message={errorMsg} />
