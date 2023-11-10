a2373be82ec194ef196ec3bdc58b9e871b5c87ec
Phil Dornfeld <phil.dornfeld@disney.com>
2023-11-10 10:15:31 -0800

    truncate

--- app/nodes/page.tsx
+++ app/nodes/page.tsx
@@ -35,7 +35,7 @@ export default async function Nodes() {
               <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                 {node.name}
               </td>
-              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+              <td className="hostname whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                 {node.hostname}
               </td>
               <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
