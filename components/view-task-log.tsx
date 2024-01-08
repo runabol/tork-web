9ad05a5a72155230a7a6b7ce00aca17cce7fde58
Arik Cohen <me@arikcohen.com>
2024-01-08 15:00:26 -0500

    Refresh logs

--- components/view-task-log.tsx
+++ components/view-task-log.tsx
@@ -2,6 +2,20 @@
 
 import { Fragment, useEffect, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
+
+function getTaskLog(id: string): Promise<string | void> {
+  return fetch(`/api/tasks/${id}/log`)
+    .then((res) => res.json() as Promise<Page<TaskLogPart>>)
+    .then((log) => {
+      return log.items
+        .map((it) => {
+          return it.contents.split("\n").reverse().join("\n");
+        })
+        .join("")
+        .trim();
+    });
+}
 
 export default function ViewTaskLog({ task }: { task: Task }) {
   const cancelButtonRef = useRef(null);
@@ -14,18 +28,7 @@ export default function ViewTaskLog({ task }: { task: Task }) {
         type="button"
         className="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-400 hover:bg-gray-50"
         onClick={() => {
-          fetch(`/api/tasks/${task.id}/log`)
-            .then((res) => res.json() as Promise<Page<TaskLogPart>>)
-            .then((log) => {
-              setContents(
-                log.items
-                  .map((it) => {
-                    return it.contents.split("\n").reverse().join("\n");
-                  })
-                  .join("")
-                  .trim()
-              );
-            });
+          getTaskLog(task.id).then((contents) => setContents(contents || ""));
           setOpen(true);
         }}
       >
@@ -69,17 +72,37 @@ export default function ViewTaskLog({ task }: { task: Task }) {
                     {contents ? contents : "no logs to show"}
                   </p>
 
-                  <button
-                    type="button"
-                    className="mt-4 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                    onClick={() => {
-                      setContents("");
-                      setOpen(false);
-                    }}
-                    ref={cancelButtonRef}
-                  >
-                    Close
-                  </button>
+                  <div className="flex gap-1 mt-4 justify-between">
+                    <button
+                      type="button"
+                      className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                      onClick={() => {
+                        setContents("");
+                        setOpen(false);
+                      }}
+                      ref={cancelButtonRef}
+                    >
+                      Close
+                    </button>
+                    {task.state === "RUNNING" ? (
+                      <button
+                        type="button"
+                        className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                        onClick={() =>
+                          getTaskLog(task.id).then((contents) =>
+                            setContents(contents || "")
+                          )
+                        }
+                      >
+                        <ArrowPathIcon
+                          className="h-5 w-5 text-black"
+                          aria-hidden="true"
+                        />
+                      </button>
+                    ) : (
+                      <></>
+                    )}
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
