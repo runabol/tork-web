54408e9d5d3d3dab403e3644b1f9560c4434d3bd
Tanvir Ahmed <mta.coder97@gmail.com>
2025-06-14 06:29:12 +0000

    fix: Fixed github action, added new not found pages and renamed a few ENV variables

--- src/app/jobs/duplicate/page.tsx
+++ src/app/jobs/duplicate/page.tsx
@@ -1,12 +1,15 @@
+import { notFound } from 'next/navigation';
+
 import DuplicateJob from '@/components/duplicate-job';
 import ENV_CONFIG from '@/config/env-config';
 import { Job, SearchParams } from '@/models';
 
 // TODO: extract this out into a service file e.g. "services/server/jobs/jobs.service.ts"
-const getJobById = async (id: string): Promise<Job> => {
+const getJobById = async (id: string): Promise<Job | null> => {
   const res = await fetch(`${ENV_CONFIG.baseUrl}/api/jobs/${id}`);
   if (!res.ok) {
-    throw new Error('Failed to fetch job.');
+    console.error(`Failed to fetch job with ID ${id}: ${res.statusText}`);
+    return null;
   }
 
   const job = await res.json();
@@ -25,6 +28,10 @@ export default async function DuplicateJobPage({ searchParams }: Props) {
   }
 
   const job = await getJobById(id as string);
+
+  if (!job) {
+    notFound();
+  }
 
   return <DuplicateJob job={job} />;
 }
